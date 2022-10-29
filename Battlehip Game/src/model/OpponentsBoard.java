4eb273c1bc14406d87d25ec0d8f60784f90dcc60
bryanYoder <91092422+bryanYoder@users.noreply.github.com>
2022-10-29 16:53:27 -0400

    Sixth commit

--- Battlehip Game/src/model/OpponentsBoard.java
+++ Battlehip Game/src/model/OpponentsBoard.java
@@ -7,7 +7,7 @@ public class OpponentsBoard extends Board {
 		int size = getSize();
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				boardArray[i][j] = new Square(i, j, true);
+				boardArray[i][j] = new Square(i, j);
 			}
 		}
 	}
